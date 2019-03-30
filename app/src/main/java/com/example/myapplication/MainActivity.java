7db3b7769cefb514427676548f24df915cd422ab
沙漏的凝滞 <1301842136@qq.com>
2019-03-30 18:06:47 +0800

    修改无法运行的bug

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,7 +6,9 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        /*drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);*/
         //NavigationView navigationView = (NavigationView)findViewById(R.id.nav_view);
         ActionBar actionBar = getSupportActionBar();
         if(actionBar != null){
@@ -28,11 +30,16 @@ public class MainActivity extends AppCompatActivity {
         System.out.print(2131231);
     }
 
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.logoff, menu);
+        return true;
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId()){
-            case android.R.id.home:
-                drawerLayout.openDrawer(GravityCompat.START);
+            case R.id.logoff_btn:
+                Toast.makeText(this, "logoff", Toast.LENGTH_SHORT);
                 break;
         }
         return true;
---
+++ app/src/main/java/com/xmut/hotel/Hotel.java
@@ -0,0 +1,13 @@
+package com.xmut.hotel;
+
+public class Hotel {
+    private String name;
+    private double price;
+    private int imageId;
+
+    public Hotel(String name, double price, int imageId){
+        this.name = name;
+        this.price = price;
+        this.imageId = imageId;
+    }
+}
