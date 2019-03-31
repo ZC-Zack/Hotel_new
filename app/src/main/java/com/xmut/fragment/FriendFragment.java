8e2b2b926e3d7c925f314586d7003f53d4eb0747
沙漏的凝滞 <1301842136@qq.com>
2019-03-31 20:36:27 +0800

    修改顶部标题被覆盖问题，待修改bug:侧滑菜单

--- app/src/main/java/com/xmut/fragment/FriendFragment.java
+++ app/src/main/java/com/xmut/fragment/FriendFragment.java
@@ -2,6 +2,8 @@ package com.xmut.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +13,13 @@ import com.example.activity.R;
 
 public class FriendFragment extends Fragment {
     private View view;
+    Toolbar toolbar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup group, Bundle bundle){
         view = inflater.inflate(R.layout.friend_layout, group, false);
+        toolbar = view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
         return  view;
     }
 }
