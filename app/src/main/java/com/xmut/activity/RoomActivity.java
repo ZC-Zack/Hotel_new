5b4d6ed70e7f61b3fea52884416f8b43b956f401
Qin <767020114@qq.com>
2019-04-08 01:48:15 +0800

    下订单功能未完善

--- app/src/main/java/com/xmut/activity/RoomActivity.java
+++ app/src/main/java/com/xmut/activity/RoomActivity.java
@@ -17,6 +17,7 @@ public class RoomActivity extends AppCompatActivity {
 
     public static final String ROOM_NAME="room_name";
     public static final String ROOM_IMAGE_ID="room_image_id";
+    public static final String ROOM_PRICE="room_price";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +25,13 @@ public class RoomActivity extends AppCompatActivity {
         setContentView(R.layout.activity_room);
         Intent intent=getIntent();
         String roomName=((Intent) intent).getStringExtra(ROOM_NAME);
+        String roomPrice=((Intent) intent).getStringExtra(ROOM_PRICE);
         int roomImageId=intent.getIntExtra(ROOM_IMAGE_ID,0);
 
         Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
         CollapsingToolbarLayout collapsingToolbar=(CollapsingToolbarLayout)findViewById(R.id.collapsing_toolbar);
         ImageView roomImageView=(ImageView)findViewById(R.id.room_image_view);
-        TextView roomContentText=(TextView)findViewById(R.id.room_content_text);
+        TextView roomContentText=(TextView)findViewById(R.id.price);
         setSupportActionBar(toolbar);
         ActionBar actionBar=getSupportActionBar();
         if(actionBar!=null){
@@ -37,13 +39,13 @@ public class RoomActivity extends AppCompatActivity {
         }
         collapsingToolbar.setTitle(roomName);
         Glide.with(this).load(roomImageId).into(roomImageView);
-        String roomContent=generateFruitContent(roomName);
+        String roomContent=generateFruitContent(roomPrice);
         roomContentText.setText(roomContent);
     }
 
     private String generateFruitContent(String roomName){
         StringBuilder roomContent=new StringBuilder();
-        for(int i=0;i<500;i++){
+        for(int i=0;i<1;i++){
             roomContent.append(roomName);
         }
         return roomContent.toString();
